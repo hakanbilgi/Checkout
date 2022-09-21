498122a68a26f1dde16d6b40fc3a471823066c67
Hakan Bilgi <hknblg34@gmail.com>
2022-09-21 21:35:09 +0300

    product

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const taxRate = 0.18;
-const shippingPrice = 15;
-const shippingFreePrice = 300;
+// const taxRate = 0.18;
+// const shippingPrice = 15;
+// const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
   //set items to LocalStorage
-  localStorage.setItem("taxRate", taxRate);
-  localStorage.setItem("shippingPrice", shippingPrice);
-  localStorage.setItem("shippingFreePrice", shippingFreePrice);
-
+  // localStorage.setItem("taxRate", taxRate);
+  // localStorage.setItem("shippingPrice", shippingPrice);
+  // localStorage.setItem("shippingFreePrice", shippingFreePrice);
   //set items to sessionStorage
-  sessionStorage.setItem("taxRate", taxRate);
-  sessionStorage.setItem("shippingPrice", shippingPrice);
-  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+  //  sessionStorage.setItem("taxRate", taxRate);
+  //  sessionStorage.setItem("shippingPrice", shippingPrice);
+  //  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const productsDiv = document.querySelector(".products");
@@ -21,18 +20,34 @@ productsDiv.addEventListener("click", (event) => {
     //console.log("minus btn is clicked!");
     if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
       event.target.parentElement.querySelector(".quantity").innerText--;
+      calculateProductPrice(event.target);
+      calculateCartPrice();
     } else {
       if (confirm("Product will be removed???")) {
         //remove
         event.target.parentElement.parentElement.parentElement.remove();
+        calculateCartPrice();
       }
     }
   } else if (event.target.classList.contains("fa-plus")) {
     //console.log("plus btn is clicked!");
     event.target.previousElementSibling.innerText++;
+    calculateProductPrice(event.target);
+    calculateCartPrice();
   } else if (event.target.className == "remove-product") {
     //console.log("remove btn is clicked!");
+    event.target.parentElement.parentElement.parentElement.remove();
+    calculateCartPrice();
   } else {
     //console.log("other element is clicked!");
   }
 });
+
+const calculateProductPrice = (clickedBtn) => {
+  const productInfoDiv = clickedBtn.parentElement.parentElement;
+  //console.log(productInfoDiv);
+  const price = productInfoDiv.querySelector(".product-price strong").innerText;
+  alert(price);
+};
+
+const calculateCartPrice = () => {};
